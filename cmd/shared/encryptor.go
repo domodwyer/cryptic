1eb685e21a09a3f92622704f4da7a31a37220407
Dom <dom@itsallbroken.com>
2017-01-29 15:43:22 +0000

    Refactor config parsing

--- cmd/shared/encryptor.go
+++ cmd/shared/encryptor.go
@@ -10,30 +10,21 @@ import (
 // GetEncryptor returns a concrete type that implements EncryptDecryptor based
 // on the configured Encryptor value in the config file.
 func GetEncryptor(config config.Encryptor) (encryptor.EncryptDecryptor, error) {
-	var enc encryptor.EncryptDecryptor
-	var err error
-
 	switch config.Encryptor() {
 	case "aes-pbkdf2":
-		enc, err = encryptor.NewKDF([]byte(config.KDFKey()))
+		return encryptor.NewKDF([]byte(config.KDFKey()))
 
 	case "aes":
-		enc, err = encryptor.NewAES([]byte(config.AESKey()), []byte(config.AESHmacKey()))
+		return encryptor.NewAES([]byte(config.AESKey()), []byte(config.AESHmacKey()))
+
 
 	case "kms":
 		if config.KMSKeyID() == "" {
-			err = errors.New("kms: No key ID set")
-		} else {
-			enc = encryptor.NewKMS(config.KMSKeyID(), config.KMSRegion())
+			return nil, errors.New("kms: No key ID set")
 		}
+		return encryptor.NewKMS(config.KMSKeyID(), config.KMSRegion()), nil
 
 	default:
-		err = errors.New("unknown decryptor")
+		return nil, errors.New("unknown decryptor")
 	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return enc, nil
 }
